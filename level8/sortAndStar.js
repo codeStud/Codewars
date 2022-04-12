9312d790974920b45bf417149a8aa1be4ff2f628
Pratik <pratik.raj0000@gmail.com>
2022-04-12 07:35:33 +0530

    Submit solution for sortAndStar

---
+++ level8/sortAndStar.js
@@ -0,0 +1,16 @@
+// You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// my solution
+function twoSort(s) {
+  let firstString = s.sort()[0];
+  return firstString.split("").join("***");
+}
+
+// other solution
+function twoSort(s) {
+  return s.sort()[0].split("").join("***");
+}
+
+const twoSort = (s) => [...s.sort()[0]].join("***");
