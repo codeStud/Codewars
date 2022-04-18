bd395ef5d71ed3cf464a91d2126b14b574eebf79
Pratik <pratik.raj0000@gmail.com>
2022-04-18 08:12:22 +0530

    Submit Solution

---
+++ level7/theHighestProfitWins.js
@@ -0,0 +1,9 @@
+// Given an array, return it's minimum and maximum value
+// minMax([1,2,3,4,5])   == [1,5]
+
+// My Solution
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+
+// Other Solutions
