b20f8ba47606d07920fcb268ee3d6d747a2f4eed
Pratik <pratik.raj0000@gmail.com>
2022-04-18 08:13:30 +0530

    Stage & Commit Deleted Files

--- level8/sumOfTwoPositiveIntegers.js
+++
@@ -1,7 +0,0 @@
-// Given an array, returns the sum of the two lowest positive numbers
-
-// My Solution
-function sumTwoSmallestNumbers(numbers) {
-  numbers.sort((a, b) => a - b);
-  return numbers[0] + numbers[1];
-}
