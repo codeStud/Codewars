c35b7a07189d90cfa56c7f70d8887afa19805f18
Pratik <pratik.raj0000@gmail.com>
2022-04-21 09:36:23 +0530

    Submit Solution

--- level7/rowWeights.js
+++ level7/rowWeights.js
@@ -1,6 +1,7 @@
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
 // My Solution
 function rowWeights(array) {
-  //your code here
   //   let teamWeights = [0, 0];
   //   array.forEach((wt, idx) => idx % 2 === 0 ? teamWeights[0] += wt : teamWeights[1] += wt);
   //   return teamWeights;
