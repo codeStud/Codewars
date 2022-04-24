90ac8f0d5746ba1fbb9e51b75c5088da4ee01533
Pratik <pratik.raj0000@gmail.com>
2022-04-24 16:44:42 +0530

    Submit Solution

---
+++ level7/evenNumbersInArray.js
@@ -0,0 +1,15 @@
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// My Solution
+function evenNumbers(array, number) {
+  let evens = [];
+  for (let i = array.length; i >= 0; i--) {
+    if (evens.length === number) {
+      return evens;
+    }
+    if (array[i] % 2 === 0) {
+      evens.unshift(array[i]);
+    }
+  }
+  return evens;
+}
