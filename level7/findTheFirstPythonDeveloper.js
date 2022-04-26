acbf6a6d189abc089b30a17eaeea7977e2f318d3
Pratik <pratik.raj0000@gmail.com>
2022-04-26 09:29:49 +0530

    Submit Solution

---
+++ level7/findTheFirstPythonDeveloper.js
@@ -0,0 +1,16 @@
+// Given an array of objects, Your task is to return one of the following strings:
+
+// < firstName here >, < country here > of the first Python developer who has signed up; or
+// There will be no Python developers if no Python developer has signed up.
+
+// My Solution
+function getFirstPython(list) {
+  let res = list.find((obj) => obj.language === "Python");
+  if (res) {
+    return `${res.firstName}, ${res.country}`;
+  } else {
+    return `There will be no Python developers`;
+  }
+}
+
+// Other Solutions
