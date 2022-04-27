c89b483f82bb38e6284b02ab2ab5f4a2a09aa948
Pratik <pratik.raj0000@gmail.com>
2022-04-27 08:50:05 +0530

    Submit Solution

--- level7/findTheAverageAge.js
+++ level7/findTheAverageAge.js
@@ -10,7 +10,7 @@ function getAverageAge(list) {
   return Math.round(avgAge / list.length);
 }
 
-// Other Solutions
+// Other Solutions...
 function getAverageAge(list) {
   return Math.round(list.reduce((sum, obj) => sum + obj.age, 0) / list.length);
 }
