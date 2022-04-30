c92670558f22c8aac8aef64cc2f2990a9b59f071
Pratik <pratik.raj0000@gmail.com>
2022-04-30 13:43:47 +0530

    Submit Solution

--- level7/NumbersToObjects.js
+++ level7/NumbersToObjects.js
@@ -5,7 +5,6 @@
 // The object key will be the number, as a string. The value will be the corresponding character code, as a string.
 
 // Return an array of the resulting objects.
-
 // All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
 
 // My Solution
