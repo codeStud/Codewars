8ed9dfaf9cf3f04da9104f1dee18cb8115a95682
Pratik <pratik.raj0000@gmail.com>
2022-05-14 08:52:13 +0530

    Submit Solution

--- level7/whatIsMyNameScore.js
+++ level7/whatIsMyNameScore.js
@@ -1,1 +1,14 @@
 // My Solution
+function nameScore(name) {
+  // alpha is the object with scores available in this function
+  let score = { [name]: 0 };
+  for (char of name) {
+    for (group in alpha) {
+      if (group.includes(char.toUpperCase())) {
+        score[name] += alpha[group];
+        break;
+      }
+    }
+  }
+  return score;
+}
