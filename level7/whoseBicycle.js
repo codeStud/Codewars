b33445424d2975a853a58ac5651be52b2d8a774f
Pratik <pratik.raj0000@gmail.com>
2022-05-15 12:35:38 +0530

    Submit Solution

--- level7/whoseBicycle.js
+++ level7/whoseBicycle.js
@@ -1,3 +1,18 @@
+// I am the father of three wonderful sons. before the beginning of the school year, I promised them that I would buy a bicycle for someone who would bring the best marks at the end of the school year. it's time to keep promises and I count on you.
+
+// You have 3 input objects(school diaries) with school subjects and marks (1-10). For example:
+
+// {
+//   'algebra': 6,
+//   'history': 8,
+//   'physics': 9,
+//   'geography': 2,
+//   'chemistry': 9
+// }
+// Return please :
+
+// 'I need to buy a bicycle for my first son.' // the sum of the marks is the highest  in the first diary.
+
 // My Solution
 function calcScore(marksheet) {
   let score = 0;
