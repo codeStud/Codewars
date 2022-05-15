93f3ad6c68433d62cdf7dbebfc325c0d4a2ea2b2
Pratik <pratik.raj0000@gmail.com>
2022-05-15 12:20:40 +0530

    Submit Solution

--- level7/playerContactManager.js
+++ level7/playerContactManager.js
@@ -1,3 +1,12 @@
+// The goal of this code is to create an array of objects that stores a player's name and contact number from a given string.
+
+// The method should return an empty array if the argument passed is an empty string or nil/None/null.
+
+// Examples
+// player_manager("John Doe, 8167238327, Jane Doe, 8163723827") returns [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+// player_manager(nil) returns []
+// player_manager("") returns []
+
 // My Solution
 function playerManager(players) {
   if (!players) {
