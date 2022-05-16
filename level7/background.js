11680d4028c57d15e4de84f6c3f8edfdbb9c2804
Pratik <pratik.raj0000@gmail.com>
2022-05-16 17:58:48 +0530

    Submit Solution

---
+++ level7/background.js
@@ -0,0 +1,20 @@
+const I = {};
+
+I.am = "a full-stack developer";
+I.love = [
+  "Family💖",
+  "Programming💻",
+  "Travelling🌏",
+  "Pizza🍕",
+  "Meeting new people👋🏻",
+];
+I.workWith = [
+  "HTML5",
+  "CSS3",
+  "Javascript",
+  "React",
+  "Node.js",
+  "Express.js",
+  "MongoDB",
+];
+I.aspire = ["to inspire others, and leave people happier than I find them."];
---
+++ level7/makeAPlayer.js
@@ -0,0 +1,9 @@
+// My Solution
+function Player(name, position, age, dribbling, pass, shoot) {
+  this.name = name;
+  this.position = position;
+  this.age = age;
+  this.dribbling = dribbling;
+  this.pass = pass;
+  this.shoot = shoot;
+}
