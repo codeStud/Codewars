f721a11f183414dd372332943df815a02940a955
Pratik <pratik.raj0000@gmail.com>
2022-05-17 18:27:13 +0530

    Submit Solution

--- level7/countStringsInObject.js
+++ level7/countStringsInObject.js
@@ -8,3 +8,24 @@
 //   fifth:  null
 //   })
 //   //returns 3
+
+// My Solution
+function strCount(obj) {
+  // Your code here
+  if (obj === null || Object.keys(obj).length === 0) return 0;
+
+  let total = 0;
+  const props = Object.keys(obj);
+  for (let keys of props) {
+    if (typeof obj[keys] !== "object") {
+      if (typeof obj[keys] === "string") {
+        total++;
+      }
+    } else {
+      let count = strCount(obj[keys]);
+      total += count;
+    }
+  }
+
+  return total;
+}
