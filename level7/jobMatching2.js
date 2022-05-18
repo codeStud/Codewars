ad8ff848380ae6aa9b07b2f1416c6deff724d36b
Pratik <pratik.raj0000@gmail.com>
2022-05-18 11:46:11 +0530

    Submit Solution

--- level7/jobMatching2.js
+++ level7/jobMatching2.js
@@ -35,3 +35,30 @@ function match(job, candidates) {
 }
 
 // Other Solutions
+const equityMatch = function (candidate, job) {
+  return candidate.desiresEquity === false || job.equityMax > 0;
+};
+
+const locationMatch = function (candidate, job) {
+  for (const location of job.locations) {
+    if (
+      candidate.desiredLocations.includes(location) ||
+      candidate.currentLocation === location
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const match = function (job, candidates) {
+  const matches = [];
+  for (const candidate of candidates) {
+    if (equityMatch(candidate, job) && locationMatch(candidate, job)) {
+      matches.push(candidate);
+    }
+  }
+
+  return matches;
+};
