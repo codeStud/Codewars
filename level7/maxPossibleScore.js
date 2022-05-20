31d24df5f9632ac5f60b4339cbb0c77d9d207340
Pratik <pratik.raj0000@gmail.com>
2022-05-20 09:13:36 +0530

    Submit Solution

--- level7/maxPossibleScore.js
+++ level7/maxPossibleScore.js
@@ -1,1 +1,9 @@
 // My Solution
+function maxPossibleScore(obj, arr) {
+  const subjects = Object.keys(obj);
+  return subjects.reduce(
+    (score, sub) =>
+      arr.toString().includes(sub) ? score + obj[sub] * 2 : score + obj[sub],
+    0
+  );
+}
