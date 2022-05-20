658b113ce6edbe7754de06378edbea9cab6c3d10
Pratik <pratik.raj0000@gmail.com>
2022-05-20 09:29:23 +0530

    Submit Solution

--- level7/getFirstAndLastArrayElement.js
+++ level7/getFirstAndLastArrayElement.js
@@ -1,3 +1,10 @@
+// Your task is to extend JavaScript Array object, with methods .first() and .last(), so you can get respectively first or last element of the array.
+
+// var a = [2, 5, 7, 3 ,4];
+
+// a.first();  // 2
+// a.last();   // 4
+
 // My Solution
 const first = function (arr) {
   if (this) return this[0];
