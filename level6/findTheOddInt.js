dd0581c0757f2630509045304dc83557bdc4f4d9
Pratik <pratik.raj0000@gmail.com>
2022-05-22 09:54:30 +0530

    Add level6 directory

---
+++ level6/findTheOddInt.js
@@ -0,0 +1,19 @@
+// Given an array of integers, find the element which occurs odd number of times
+
+// My Solution
+function findOdd(A) {
+  const countObj = A.reduce((obj, el) => {
+    if (el in obj) {
+      obj[el]++;
+    } else {
+      obj[el] = 1;
+    }
+    return obj;
+  }, {});
+
+  for (let key in countObj) {
+    if (countObj[key] % 2 !== 0) {
+      return +key;
+    }
+  }
+}
