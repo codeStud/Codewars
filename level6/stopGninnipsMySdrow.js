7fb93af9c16ac63fbcce37554579775cb7d32b13
Pratik <pratik.raj0000@gmail.com>
2022-05-22 10:06:25 +0530

    Submit Solution

--- level6/stopGninnipsMySdrow.js
+++ level6/stopGninnipsMySdrow.js
@@ -21,3 +21,11 @@ function spinWords(string) {
 }
 
 // Other Solutions
+function spinWords(words) {
+  return words
+    .split(" ")
+    .map(function (word) {
+      return word.length > 4 ? word.split("").reverse().join("") : word;
+    })
+    .join(" ");
+}
