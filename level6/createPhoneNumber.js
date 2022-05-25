cd9232b3c472f29d0b3cfd289e9e1f13f7d5a766
Pratik <pratik.raj0000@gmail.com>
2022-05-25 08:59:32 +0530

    Submit Solution

--- level6/createPhoneNumber.js
+++ level6/createPhoneNumber.js
@@ -10,3 +10,14 @@ function createPhoneNumber(numbers) {
   phoneNumber += numbers.slice(6).join("");
   return phoneNumber;
 }
+
+// Other Solutions
+function createPhoneNumber(numbers) {
+  var format = "(xxx) xxx-xxxx";
+
+  for (var i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i]);
+  }
+
+  return format;
+}
