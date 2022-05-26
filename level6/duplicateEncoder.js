7b2f2dbc6a25b08eedeaa2cc678d0bf9052c4120
Pratik <pratik.raj0000@gmail.com>
2022-05-26 11:40:14 +0530

    Submit Solution

--- level6/duplicateEncoder.js
+++ level6/duplicateEncoder.js
@@ -31,3 +31,12 @@ function duplicateEncode(word) {
 }
 
 // Other Solutions
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
