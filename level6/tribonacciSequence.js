8a01af0c9449688eb4405e2bd0a4183f7de05197
Pratik <pratik.raj0000@gmail.com>
2022-05-29 09:56:03 +0530

    Submit Solution

---
+++ level6/tribonacciSequence.js
@@ -0,0 +1,21 @@
+// Given a signature consisting of 3 numbers, return the tribonacci sequence till nth number.
+// Tribonacci sequence means sum of last 3 digits
+
+// My Solution
+function tribonacci(signature, n) {
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [signature[0]];
+  } else if (n === 2) {
+    return signature.slice(0, n);
+  } else if (n === 3) {
+    return signature.slice(0, n);
+  } else {
+    let arr = signature.slice(0, 3);
+    for (let i = 3; i < n; i++) {
+      arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+    }
+    return arr;
+  }
+}
