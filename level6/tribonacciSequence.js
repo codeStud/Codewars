65632c07a562b3df8ac875960c4fea391348116a
Pratik <pratik.raj0000@gmail.com>
2022-05-29 09:57:24 +0530

    Submit Solution

--- level6/tribonacciSequence.js
+++ level6/tribonacciSequence.js
@@ -21,3 +21,10 @@ function tribonacci(signature, n) {
 }
 
 // Other Solutions
+function tribonacci(signature, n) {
+  const result = signature.slice(0, n);
+  while (result.length < n) {
+    result[result.length] = result.slice(-3).reduce((p, c) => p + c, 0);
+  }
+  return result;
+}
