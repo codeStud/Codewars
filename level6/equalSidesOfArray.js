65783df5684dbf63576952227c4c50de331a21a0
Pratik <pratik.raj0000@gmail.com>
2022-05-30 18:42:29 +0530

    Submit Solution

--- level6/equalSidesOfArray.js
+++ level6/equalSidesOfArray.js
@@ -5,10 +5,6 @@
 // Let's say you are given the array {1,2,3,4,3,2,1}:
 // Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
 
-// Let's look at another one.
-// You are given the array {1,100,50,-51,1,1}:
-// Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
-
 // Last one:
 // You are given the array {20,10,-80,10,10,15,35}
 // At index 0 the left side is {}
@@ -83,3 +79,5 @@ function findEvenIndex(arr) {
 
   return -1;
 }
+
+// Other Solutions
