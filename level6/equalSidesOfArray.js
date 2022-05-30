fee3fb559af051789628ba3bcb529f1eb59a595f
Pratik <pratik.raj0000@gmail.com>
2022-05-30 18:43:47 +0530

    Submit Solution

--- level6/equalSidesOfArray.js
+++ level6/equalSidesOfArray.js
@@ -81,3 +81,13 @@ function findEvenIndex(arr) {
 }
 
 // Other Solutions
+function findEvenIndex(arr) {
+  let left = 0;
+  let right = arr.reduce((s, n) => s + n, 0);
+  for (let i = 0; i < arr.length; i++) {
+    right -= arr[i];
+    if (left === right) return i;
+    left += arr[i];
+  }
+  return -1;
+}
