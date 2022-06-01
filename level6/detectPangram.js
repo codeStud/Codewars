d952237ae0f448f609b6fa5e11f73c076ccc59d1
Pratik <pratik.raj0000@gmail.com>
2022-06-01 08:46:34 +0530

    Submit Solution

--- level6/detectPangram.js
+++ level6/detectPangram.js
@@ -46,3 +46,9 @@ function isPangram(string) {
 }
 
 // Other Solutions
+function isPangram(string) {
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
+}
