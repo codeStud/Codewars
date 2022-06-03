8a29b28c35c95691b5cee3f73e12e27a8856efc7
Pratik <pratik.raj0000@gmail.com>
2022-06-03 08:34:07 +0530

    Submit Solution

--- level6/sortTheOdds.js
+++ level6/sortTheOdds.js
@@ -1,3 +1,9 @@
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+
 // My Solution
 function sortArray(array) {
   let odds = [];
