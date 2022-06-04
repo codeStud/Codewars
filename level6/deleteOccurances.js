7bbec4e94c7334635de7061ab050fd1fdbe3bc5d
Pratik <pratik.raj0000@gmail.com>
2022-06-04 08:51:25 +0530

    Submit Solution

--- level6/deleteOccurances.js
+++ level6/deleteOccurances.js
@@ -1,1 +1,23 @@
 // My Solution
+function deleteNth(arr, n) {
+  const res = [];
+  const count = {};
+  // store the count of each number in the arr
+  for (let i = 0; i < arr.length; i++) {
+    if (count.hasOwnProperty(arr[i])) {
+      count[arr[i]] += 1;
+      // push if the count is <= n
+      if (count[arr[i]] <= n) {
+        res.push(arr[i]);
+      }
+    } else {
+      count[arr[i]] = 1;
+      // push if the count is <= n
+      if (count[arr[i]] <= n) {
+        res.push(arr[i]);
+      }
+    }
+  }
+
+  return res;
+}
