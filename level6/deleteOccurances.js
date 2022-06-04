0261dc8456ec8a2b0a28d80d2f2a4d7794d3f44d
Pratik <pratik.raj0000@gmail.com>
2022-06-04 08:54:02 +0530

    Submit Solution

--- level6/deleteOccurances.js
+++ level6/deleteOccurances.js
@@ -34,3 +34,11 @@ function deleteNth(arr, x) {
     return cache[n] <= x;
   });
 }
+
+function deleteNth(arr, x) {
+  var obj = {};
+  return arr.filter(function (number) {
+    obj[number] = obj[number] ? obj[number] + 1 : 1;
+    return obj[number] <= x;
+  });
+}
