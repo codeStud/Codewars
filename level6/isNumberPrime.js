31cabf320c33298e8a78beb8418027718c75c6f5
Pratik <pratik.raj0000@gmail.com>
2022-06-07 10:45:57 +0530

    Submit Solution

--- level6/isNumberPrime.js
+++ level6/isNumberPrime.js
@@ -17,3 +17,13 @@ function isPrime(num) {
 }
 
 // Other Solutions
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
