a4e4e01d65f2df7f7176a42dc2ee888ee07d3979
Pratik <pratik.raj0000@gmail.com>
2022-06-07 09:37:03 +0530

    Submit Solution

--- level6/validBraces.js
+++ level6/validBraces.js
@@ -56,3 +56,25 @@ function validBraces(braces) {
 
   return stack.length === 0; // any unclosed braces left?
 }
+
+function validBraces(braces) {
+  for (var i = 0, depth = []; i < braces.length; i++) {
+    switch (braces[i]) {
+      case "(":
+      case "[":
+      case "{":
+        depth.push(braces[i]);
+        break;
+      case ")":
+        if (depth.pop() != "(") return false;
+        break;
+      case "]":
+        if (depth.pop() != "[") return false;
+        break;
+      case "}":
+        if (depth.pop() != "{") return false;
+        break;
+    }
+  }
+  return depth.length == 0;
+}
