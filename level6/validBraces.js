819037e78e39a227f5197078854cd16e656d6a9f
Pratik <pratik.raj0000@gmail.com>
2022-06-07 09:36:01 +0530

    Submit Solution

--- level6/validBraces.js
+++ level6/validBraces.js
@@ -35,3 +35,24 @@ function validBraces(braces) {
 }
 
 // Other Solutions
+function validBraces(braces) {
+  var matches = { "(": ")", "{": "}", "[": "]" };
+  var stack = [];
+  var currentChar;
+
+  for (var i = 0; i < braces.length; i++) {
+    currentChar = braces[i];
+
+    if (matches[currentChar]) {
+      // opening braces
+      stack.push(currentChar);
+    } else {
+      // closing braces
+      if (currentChar !== matches[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; // any unclosed braces left?
+}
