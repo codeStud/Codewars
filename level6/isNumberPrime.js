194ac4fc21a8eabacfa9a9b573ffa13bd6cf53b0
Pratik <pratik.raj0000@gmail.com>
2022-06-07 10:44:58 +0530

    Submit Solution

---
+++ level6/isNumberPrime.js
@@ -0,0 +1,17 @@
+// My Solution
+function isPrime(num) {
+  if (num > 1) {
+    let factorCount = 1;
+    for (let i = 1; i * i <= num; i++) {
+      if (factorCount > 2) {
+        return false;
+      }
+      if (num % i === 0) {
+        factorCount++;
+      }
+    }
+    return factorCount <= 2;
+  } else {
+    return false;
+  }
+}
