daf542dc8c64e6944d8f31b7d0df2a279c804ec4
Pratik <pratik.raj0000@gmail.com>
2022-06-08 09:11:29 +0530

    Submit Solution

--- level6/areTheyTheSame.js
+++ level6/areTheyTheSame.js
@@ -22,3 +22,11 @@ function comp(array1, array2) {
   // check for extra remaining elements in array2
   return array2.length === 0;
 }
+
+// Other Solutions
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map((v) => v * v).every((v, i) => v == array2[i]);
+}
