519c434b656ecf095cc5b8dcb6866acb4b09d562
Pratik <pratik.raj0000@gmail.com>
2022-06-09 08:07:53 +0530

    Submit Solution

--- level6/writeNumberInExpandedForm.js
+++ level6/writeNumberInExpandedForm.js
@@ -25,3 +25,12 @@ function expandedForm(num) {
 }
 
 // Other Solutions
+const expandedForm = (n) =>
+  n
+    .toString()
+    .split("")
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(" + ");
