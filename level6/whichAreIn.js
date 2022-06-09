38af4d5677e20fbed890211190d76f6aff697a98
Pratik <pratik.raj0000@gmail.com>
2022-06-09 08:21:42 +0530

    Submit Solution

---
+++ level6/whichAreIn.js
@@ -0,0 +1,12 @@
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// NO DUPLICATES
+
+// My Solution
