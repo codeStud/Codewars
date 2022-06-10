784b37e3f34148146abc0f904ae37aa321a58106
Pratik <pratik.raj0000@gmail.com>
2022-06-10 13:06:44 +0530

    Submit Solution

--- level6/bouncingBalls.js
+++ level6/bouncingBalls.js
@@ -39,7 +39,6 @@ function bouncingBall(h, bounce, window) {
 function bouncingBall(h, bounce, window) {
   if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
   let seen = 0;
-
   while (h > window) {
     seen += 1;
     h *= bounce;
