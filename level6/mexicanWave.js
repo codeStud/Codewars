376e3fdda0d248f06485268356e18751720edc6a
Pratik <pratik.raj0000@gmail.com>
2022-06-11 10:02:50 +0530

    Submit Solution

--- level6/mexicanWave.js
+++ level6/mexicanWave.js
@@ -1,1 +1,15 @@
 // My Solution
+function wave(str) {
+  // Code here
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toUpperCase() !== str[i].toLowerCase()) {
+      let temp = str.slice(0, i);
+      temp += str[i].toUpperCase();
+      temp += str.slice(i + 1);
+      arr.push(temp);
+    }
+  }
+
+  return arr;
+}
