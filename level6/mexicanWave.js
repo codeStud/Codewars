a4b5e3abc78441a5e214d40a2a782fcd74867914
Pratik <pratik.raj0000@gmail.com>
2022-06-11 10:04:36 +0530

    Submit Solution

--- level6/mexicanWave.js
+++ level6/mexicanWave.js
@@ -31,3 +31,15 @@ function wave(str) {
 
   return result;
 }
+
+function wave(str) {
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split("");
+    if (copy[i] !== " ") {
+      copy[i] = copy[i].toUpperCase();
+      newArr.push(copy.join(""));
+    }
+  }
+  return newArr;
+}
