117238283e552419dd9631957bf7780d1d405a3d
Pratik <pratik.raj0000@gmail.com>
2022-06-11 10:04:19 +0530

    Submit Solution

--- level6/mexicanWave.js
+++ level6/mexicanWave.js
@@ -18,3 +18,16 @@ function wave(str) {
 }
 
 // Other Solutions
+function wave(str) {
+  let result = [];
+
+  str.split("").forEach((char, index) => {
+    if (/[a-z]/.test(char)) {
+      result.push(
+        str.slice(0, index) + char.toUpperCase() + str.slice(index + 1)
+      );
+    }
+  });
+
+  return result;
+}
