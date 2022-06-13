7f1f78c9c82ae84528a357c5c08cf7ea070698fe
Pratik <pratik.raj0000@gmail.com>
2022-06-13 11:31:59 +0530

    Submit Solution

--- level6/romanNumeralsEncoder.js
+++ level6/romanNumeralsEncoder.js
@@ -1,2 +1,30 @@
 // My Solution
 // None
+// Other Solutions
+
+function solution(number) {
+  var map = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    },
+    output = "";
+
+  for (var i in map) {
+    while (number >= map[i]) {
+      output += i;
+      number -= map[i];
+    }
+  }
+  return output;
+}
