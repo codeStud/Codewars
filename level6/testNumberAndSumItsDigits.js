01ada9aa9f7b74c01b1b6032a6f92f7db11c57a1
Pratik <pratik.raj0000@gmail.com>
2022-06-13 11:16:21 +0530

    Submit Solution

--- level6/testNumberAndSumItsDigits.js
+++ level6/testNumberAndSumItsDigits.js
@@ -27,3 +27,17 @@ function sumDigPow(a, b) {
 }
 
 // Other Solutions
+function sumDigPow(a, b) {
+  var ans = [];
+  while (a <= b) {
+    if (
+      a
+        .toString()
+        .split("")
+        .reduce((x, y, i) => x + (+y) ** (i + 1), 0) == a
+    )
+      ans.push(a);
+    a++;
+  }
+  return ans;
+}
