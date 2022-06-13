1cbe574265c355a07572516e1e15b2045f085f33
Pratik <pratik.raj0000@gmail.com>
2022-06-13 11:14:54 +0530

    Submit Solution

--- level6/testNumberAndSumItsDigits.js
+++ level6/testNumberAndSumItsDigits.js
@@ -1,7 +1,6 @@
 // The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
 
 // In effect: 89 = 8^1 + 9^2
-
 // The next number in having this property is 135.
 
 // See this property again: 135 = 1^1 + 3^2 + 5^3
