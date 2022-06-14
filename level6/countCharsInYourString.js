ac6f28a27537c5b72dc08e14663a059197afcd2a
Pratik <pratik.raj0000@gmail.com>
2022-06-14 08:23:17 +0530

    Submit Solution

--- level6/countCharsInYourString.js
+++ level6/countCharsInYourString.js
@@ -13,3 +13,16 @@ function count(string) {
 }
 
 // Other Solutions
+function count(string) {
+  var result = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (result.hasOwnProperty(string[i])) {
+      result[string[i]] += 1;
+    } else {
+      result[string[i]] = 1;
+    }
+  }
+
+  return result;
+}
