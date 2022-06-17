96cc1b4939de3adcb551d3e853ef658b060e75da
Pratik <pratik.raj0000@gmail.com>
2022-06-17 09:48:28 +0530

    Submit Solution

--- level6/rectangleIntoSquares.js
+++ level6/rectangleIntoSquares.js
@@ -19,3 +19,14 @@ function sqInRect(lng, wdth) {
 
   return res;
 }
+
+// Other Solutions
+function sqInRect(lng, wdth) {
+  let arr = [];
+  if (lng === wdth) return null;
+  while (lng > 0 && wdth > 0) {
+    arr.push(lng > wdth ? wdth : lng);
+    lng > wdth ? (lng -= wdth) : (wdth -= lng);
+  }
+  return arr;
+}
