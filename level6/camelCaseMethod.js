edb928d9327c300ee1a6b128792828ecc6fcf7ba
Pratik <pratik.raj0000@gmail.com>
2022-06-17 09:06:37 +0530

    Submit Solution

--- level6/camelCaseMethod.js
+++ level6/camelCaseMethod.js
@@ -17,3 +17,10 @@ String.prototype.camelCase = function () {
 };
 
 // Other Solutions
+String.prototype.camelCase = function () {
+  return this.split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join("");
+};
