061abf6404a7280cb45b297c98dfe702bd80e020
Pratik <pratik.raj0000@gmail.com>
2022-06-18 12:20:50 +0530

    Submit Solution

--- level6/weirdStringCase.js
+++ level6/weirdStringCase.js
@@ -20,3 +20,14 @@ function toWeirdCase(string) {
 }
 
 // Other Solutions
+function toWeirdCaseCharacter(chr, index) {
+  return index % 2 ? chr.toLowerCase() : chr.toUpperCase();
+}
+
+function toWeirdCaseWord(word) {
+  return word.split("").map(toWeirdCaseCharacter).join("");
+}
+
+function toWeirdCase(string) {
+  return string.split(" ").map(toWeirdCaseWord).join(" ");
+}
