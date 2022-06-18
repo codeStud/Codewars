3e4e8edc8573c42282601b0c1fd2f638a3cea86a
Pratik <pratik.raj0000@gmail.com>
2022-06-18 12:48:34 +0530

    Submit Solution

--- level6/giveMeAdiamond.js
+++ level6/giveMeAdiamond.js
@@ -1,3 +1,7 @@
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
 // My Solution
 function diamond(n) {
   if (n < 3 || n % 2 === 0) return null;
