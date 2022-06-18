cf35fe083a38d7f304c9f537335c64ed5ddfa2ba
Pratik <pratik.raj0000@gmail.com>
2022-06-18 12:49:29 +0530

    Submit Solution

--- level6/giveMeAdiamond.js
+++ level6/giveMeAdiamond.js
@@ -48,3 +48,16 @@ function diamond(n) {
 
   return diam;
 }
+
+// Other Solutions
+function diamond(n) {
+  if (n < 0 || n % 2 == 0) return null;
+  for (var i = 0, diamond = ""; i < n; i++) {
+    var stars = n - Math.abs(n - 2 * i - 1);
+    var spaces = (n - stars) / 2;
+    while (spaces-- > 0) diamond += " ";
+    while (stars-- > 0) diamond += "*";
+    diamond += "\n";
+  }
+  return diamond;
+}
