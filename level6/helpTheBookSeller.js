fd8963a34628b59d58d36a4a8ea84d46f833e94d
Pratik <pratik.raj0000@gmail.com>
2022-06-20 09:12:15 +0530

    Submit Solution

--- level6/helpTheBookSeller.js
+++ level6/helpTheBookSeller.js
@@ -41,3 +41,20 @@ function stockList(listOfArt, listOfCat) {
 
   return resStr.slice(0, resStr.length - 3);
 }
+
+// Other Solutions
+function stockList(listOfArt, listOfCat) {
+  var qs = {};
+  if (!listOfArt.length) return "";
+
+  listOfArt.forEach(function (art) {
+    var cat = art[0];
+    qs[cat] = (qs[cat] | 0) + +art.split(" ")[1];
+  });
+
+  return listOfCat
+    .map(function (c) {
+      return "(" + c + " : " + (qs[c] | 0) + ")";
+    })
+    .join(" - ");
+}
