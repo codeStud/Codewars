446635b5a548cb8acf5b541fcb7248b4aa2d0c38
Pratik <pratik.raj0000@gmail.com>
2022-06-23 09:17:37 +0530

    Submit Solution

--- level6/sumOfParts.js
+++ level6/sumOfParts.js
@@ -1,3 +1,17 @@
+// Let us consider this example (array written in general format):
+
+// ls = [0, 1, 3, 6, 10]
+
+// Its following parts:
+
+// ls = [0, 1, 3, 6, 10]
+// ls = [1, 3, 6, 10]
+// ls = [3, 6, 10]
+// ls = [6, 10]
+// ls = [10]
+// ls = []
+// The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
 // My Solution
 function partsSums(ls) {
   let partsSum = [];
