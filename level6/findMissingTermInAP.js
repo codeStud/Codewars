e6a1812bc35c5e93acc6b9ba9813653467a15f35
Pratik <pratik.raj0000@gmail.com>
2022-06-25 21:37:56 +0530

    Submit Solution

--- level6/findMissingTermInAP.js
+++ level6/findMissingTermInAP.js
@@ -16,3 +16,11 @@ var findMissing = function (list) {
 };
 
 // Other Solutions
+var findMissing = function (list) {
+  var step = (list[list.length - 1] - list[0]) / list.length;
+  return (
+    list.filter(function (val, index) {
+      return val !== list[0] + index * step;
+    })[0] - step
+  );
+};
