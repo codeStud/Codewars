e9ca28eaa22b9b5eb0081bb738024dea9ae48d22
Pratik <pratik.raj0000@gmail.com>
2022-06-25 21:29:17 +0530

    Submit Solution

--- level6/findMissingTermInAP.js
+++ level6/findMissingTermInAP.js
@@ -1,1 +1,9 @@
 // My Solution
+var findMissing = function (list) {
+  let d = [list[1] - list[0], list[2] - list[1]];
+  let commonDiff = Math.min(...d) < 0 ? Math.max(...d) : Math.min(...d);
+  const oddOne = list.find(
+    (el, idx, list) => idx !== 0 && list[idx] - list[idx - 1] !== commonDiff
+  );
+  return oddOne - commonDiff;
+};
