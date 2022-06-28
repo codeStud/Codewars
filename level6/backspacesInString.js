9fe09c2fbd40b4dc676c30f949939315fd3e8503
Pratik <pratik.raj0000@gmail.com>
2022-06-28 08:44:11 +0530

    Submit Solution

--- level6/backspacesInString.js
+++ level6/backspacesInString.js
@@ -23,3 +23,8 @@ function cleanString(s) {
 }
 
 // Other Solutions
+function cleanString(s) {
+  let result = [];
+  [...s].map((char) => (char === "#" ? result.pop() : result.push(char)));
+  return result.join("");
+}
