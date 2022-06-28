45adee2bd4363063a0f330f30d6e6aee7f14c6a1
Pratik <pratik.raj0000@gmail.com>
2022-06-28 08:43:15 +0530

    Submit Solution

--- level6/backspacesInString.js
+++ level6/backspacesInString.js
@@ -1,3 +1,13 @@
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
 // My Solution
 function cleanString(s) {
   let stack = [];
