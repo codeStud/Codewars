6c1d5cfd9b5404be4389d908b4fb4011f7629281
Pratik <pratik.raj0000@gmail.com>
2022-06-29 09:55:00 +0530

    Submit Solution

--- level5/simplePigLatin.js
+++ level5/simplePigLatin.js
@@ -23,3 +23,12 @@ function pigIt(str) {
 }
 
 // Other Solutions
+function pigIt(str) {
+  var arrayWord = str.split(" ");
+  return arrayWord
+    .map(function (word) {
+      var firstLetter = word.charAt(0);
+      return word.slice(1) + firstLetter + "ay";
+    })
+    .join(" ");
+}
