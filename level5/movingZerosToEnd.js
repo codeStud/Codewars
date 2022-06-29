003f3a9b1c5abdf0cc6668f1b8d603e84c635c30
Pratik <pratik.raj0000@gmail.com>
2022-06-29 10:08:00 +0530

    Submit Solution

---
+++ level5/movingZerosToEnd.js
@@ -0,0 +1,13 @@
+// My Solution
+function moveZeros(arr) {
+  // PREP
+  // since you are modifying the array concurrently, loop the array backwards
+  // otherwise you might miss elements For eg - [ 9, 0, 0, 2] would give [9, 0, 2, 0]
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  }
+  return arr;
+}
