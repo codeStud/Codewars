acd35bac6f2a13b6b6aad5bca2e42cadddd5a05d
Pratik <pratik.raj0000@gmail.com>
2022-06-30 14:39:08 +0530

    Submit Solution

--- level5/validParentheses.js
+++ level5/validParentheses.js
@@ -25,3 +25,13 @@ function validParentheses(parens) {
 }
 
 // Other Solutions
+function validParentheses(parens) {
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == "(") n++;
+    if (parens[i] == ")") n--;
+    if (n < 0) return false;
+  }
+
+  return n == 0;
+}
