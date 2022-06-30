4bf129aaaec0a1d11c98f0f2c57bfcf0a02ebde9
Pratik <pratik.raj0000@gmail.com>
2022-06-30 14:38:07 +0530

    Submit Solution

--- level5/validParentheses.js
+++ level5/validParentheses.js
@@ -1,3 +1,11 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
 // My Solution
 function validParentheses(parens) {
   // assuming only valid chars '(' &')' given in parens string
