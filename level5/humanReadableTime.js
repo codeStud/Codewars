493f95c46b1100880fa2b437b56246d01029c6ca
Pratik <pratik.raj0000@gmail.com>
2022-06-30 14:52:29 +0530

    Submit Solution

--- level5/humanReadableTime.js
+++ level5/humanReadableTime.js
@@ -22,3 +22,14 @@ function humanReadable(seconds) {
 }
 
 // Other Solutions
+function humanReadable(seconds) {
+  var hours = parseInt(seconds / 3600);
+  var minutes = parseInt(seconds / 60) % 60;
+  var seconds = seconds % 60;
+
+  var pad = function (val) {
+    return val < 10 ? "0" + val : val;
+  };
+
+  return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+}
