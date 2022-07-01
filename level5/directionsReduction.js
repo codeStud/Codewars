016585364f4b1441fc1a0eef55440268a8a49742
Pratik <pratik.raj0000@gmail.com>
2022-07-01 09:14:59 +0530

    Submit Solution

--- level5/directionsReduction.js
+++ level5/directionsReduction.js
@@ -1,3 +1,8 @@
+// Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
+
+// 1. ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"] -> ['WEST']
+// 2. In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
 // My Solution
 function dirReduc(arr) {
   let opp = { NORTH: "SOUTH", SOUTH: "NORTH", EAST: "WEST", WEST: "EAST" };
