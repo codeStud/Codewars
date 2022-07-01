692eb8268b7062f22c47fe4172cde964379d83b4
Pratik <pratik.raj0000@gmail.com>
2022-07-01 09:15:53 +0530

    Submit Solution

--- level5/directionsReduction.js
+++ level5/directionsReduction.js
@@ -20,3 +20,16 @@ function dirReduc(arr) {
 }
 
 // Other Solutions
+function dirReduc(plan) {
+  var opposite = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return plan.reduce(function (dirs, dir) {
+    if (dirs[dirs.length - 1] === opposite[dir]) dirs.pop();
+    else dirs.push(dir);
+    return dirs;
+  }, []);
+}
