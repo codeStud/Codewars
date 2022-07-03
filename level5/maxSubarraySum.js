d8ddade8a299aaabb22f10b98c8df9f900cca52e
Pratik <pratik.raj0000@gmail.com>
2022-07-03 17:39:18 +0530

    Submit Solution

--- level5/maxSubarraySum.js
+++ level5/maxSubarraySum.js
@@ -23,3 +23,10 @@ var maxSequence = function (arr) {
 };
 
 // Other Solutions
+var maxSequence = function (arr) {
+  var currentSum = 0;
+  return arr.reduce(function (maxSum, number) {
+    currentSum = Math.max(currentSum + number, 0);
+    return Math.max(currentSum, maxSum);
+  }, 0);
+};
