49a90970a378a3ca585f062493f285b74639900d
Pratik <pratik.raj0000@gmail.com>
2022-07-03 18:17:14 +0530

    Submit Solution

--- level5/productOfConsecutiveFibNumbers.js
+++ level5/productOfConsecutiveFibNumbers.js
@@ -1,3 +1,17 @@
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
 // My Solution
 function productFib(prod) {
   // keeping only last two fibonacci numbers
