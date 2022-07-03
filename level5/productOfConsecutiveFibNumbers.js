c5cbc29224bd8e9bb842754524448e9fd4764cb5
Pratik <pratik.raj0000@gmail.com>
2022-07-03 18:17:47 +0530

    Submit Solution

--- level5/productOfConsecutiveFibNumbers.js
+++ level5/productOfConsecutiveFibNumbers.js
@@ -37,3 +37,14 @@ function productFib(prod) {
 
   return res;
 }
+
+// Other Solutions
+function productFib(prod) {
+  var n = 0;
+  var nPlus = 1;
+  while (n * nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n * nPlus === prod];
+}
