d84e6e1260e50ed062b3e87d2d29a3094f728e47
Pratik <pratik.raj0000@gmail.com>
2022-07-04 08:59:11 +0530

    Submit Solution

--- level5/weightForWeight.js
+++ level5/weightForWeight.js
@@ -1,3 +1,19 @@
+// The weight of a number will be from now on the sum of its digits.
+
+// For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+
+// Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+
+// Example:
+// "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
+
+// "100 180 90 56 65 74 68 86 99"
+// When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+
+// 180 is before 90 since, having the same "weight" (9), it comes before as a string.
+
+// All numbers in the list are positive numbers and the list can be empty.
+
 // My Solution
 function orderWeight(strng) {
   if (strng.length === 0) {
