3d2643bfb18ce2a6275b5a74d32f63a75e11c461
Pratik <pratik.raj0000@gmail.com>
2022-07-05 08:59:12 +0530

    Submit Solution

--- level5/theHashtagGenerator.js
+++ level5/theHashtagGenerator.js
@@ -35,3 +35,12 @@ function generateHashtag(str) {
 }
 
 // Other Solutions
+function generateHashtag(str) {
+  return str.length > 140 || str === ""
+    ? false
+    : "#" + str.split(" ").map(capitalize).join("");
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
