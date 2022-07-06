2483106b759bb4455c4e1ba3b20f3e5e22c58cf4
Pratik <pratik.raj0000@gmail.com>
2022-07-06 08:42:55 +0530

    Submit Solution

--- level5/scramblies.js
+++ level5/scramblies.js
@@ -1,1 +1,14 @@
 // My Solution
+
+// Other Solution
+function scramble(str1, str2) {
+  // assuming ascii characters only
+  var hashtab = [...new Array(256)].map((x) => 0);
+
+  str2.split("").forEach((ele) => hashtab[ele.charCodeAt()]++);
+  str1.split("").forEach((ele) => hashtab[ele.charCodeAt()]--);
+
+  hashtab = hashtab.filter((x) => x > 0);
+
+  return hashtab.length == 0;
+}
