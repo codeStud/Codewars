7fb6ff959e78338740c5ecbaf783c09825d5544a
Pratik <pratik.raj0000@gmail.com>
2022-07-09 07:24:31 +0530

    Submit Solution

---
+++ level5/primesInNumbers.js
@@ -0,0 +1,16 @@
+// My Solution
+if (n < 2) return `(${n})`;
+let factors = "";
+for (let i = 2; i <= n; i++) {
+  let count = 0;
+  while (n % i === 0) {
+    count++;
+    n /= i;
+  }
+  if (count) {
+    factors += `(${i}`;
+    if (count > 1) factors += `**${count}`;
+    factors += `)`;
+  }
+}
+return factors;
