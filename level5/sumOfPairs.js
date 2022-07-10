6252152a73dd8d291e12df6c04a09fd4165e8939
Pratik <pratik.raj0000@gmail.com>
2022-07-10 13:35:05 +0530

    Submit Solution

--- level5/sumOfPairs.js
+++ level5/sumOfPairs.js
@@ -35,3 +35,10 @@ function sumPairs(ints, s) {
 }
 
 // Other Solutions
+function sum_pairs(ints, s) {
+  let seen = new Set();
+  for (let i of ints) {
+    if (seen.has(s - i)) return [s - i, i];
+    seen.add(i);
+  }
+}
