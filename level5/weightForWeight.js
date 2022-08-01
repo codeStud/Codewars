ddce55179e15813aac822683577279e359601774
Pratik <pratik.raj0000@gmail.com>
2022-08-01 21:03:40 +0530

    Submit Solution

--- level5/weightForWeight.js
+++ level5/weightForWeight.js
@@ -1,5 +1,4 @@
 // The weight of a number will be from now on the sum of its digits.
-
 // For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
 
 // Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
