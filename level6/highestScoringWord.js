4ff7b2f8aea3bd4afa90713cfd18b5fd36c2b0e2
Pratik <pratik.raj0000@gmail.com>
2022-08-16 21:08:08 +0530

    Modify Solution

--- level6/highestScoringWord.js
+++ level6/highestScoringWord.js
@@ -1,4 +1,5 @@
 // Given a string of words, you need to find the highest scoring word.
+
 // Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
 
 // You need to return the highest scoring word as a string.
