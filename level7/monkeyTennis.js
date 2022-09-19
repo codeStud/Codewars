662e39febc818693fe67818193875d191a75f3da
Pratik <pratik.raj0000@gmail.com>
2022-09-19 19:35:38 +0530

    Modify Solution

--- level7/monkeyTennis.js
+++ level7/monkeyTennis.js
@@ -1,5 +1,4 @@
 // You will receive an array of tennis balls and banana peels, which will each be represented by their weight. Tennis balls weigh exactly 58 grams, and banana peels weigh 60 grams upwards.
-
 // Your job is to gather up just the tennis balls and return them in a sack. The sack will be represented by an object that has a 'weight' property, the value of which should be equal to the combined weight of all tennis balls in the array. If the array contains no tennis balls, then the weight value of the returned sack object should be 0.
 
 function ballCollector(detritus) {
