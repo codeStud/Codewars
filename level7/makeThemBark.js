d4f5f3172c4e4a310e3b16f549553429350ec99f
Pratik <pratik.raj0000@gmail.com>
2022-10-08 19:44:19 +0530

    Modify Solution

--- level7/makeThemBark.js
+++ level7/makeThemBark.js
@@ -1,5 +1,4 @@
 // The dogbreeder says he wont pay you, until he can make every dog object .bark(). Even the ones already done with your constructor. "Every dog barks" he says. He also refuses to rewrite them, lazy as he is.
-
 // You can't even count how much objects that bastard client of yours already made. He has a lot of dogs, and none of them can .bark().
 
 // Can you solve this problem, or will you let this client outsmart you for good?
