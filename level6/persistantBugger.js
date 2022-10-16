265a70a3533c4b95d10a78d63e679c0fb8621060
Pratik <pratik.raj0000@gmail.com>
2022-10-16 20:05:44 +0530

    Submit Solution

--- level6/persistantBugger.js
+++ level6/persistantBugger.js
@@ -1,5 +1,4 @@
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
-
 // For example (Input --> Output):
 
 // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
