99fc9de5674cd4bc9eedf6fc671dadeafebc9aeb
Pratik <pratik.raj0000@gmail.com>
2022-11-10 20:50:12 +0530

    Submit Solution

--- level6/areTheyTheSame.js
+++ level6/areTheyTheSame.js
@@ -1,4 +1,5 @@
 // Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
 // Examples
 // Valid arrays
 // a = [121, 144, 19, 161, 19, 144, 19, 11]
